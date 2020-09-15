491fe79e00ae0c6130f33b2a0ee381565132bc27
anderulf <ulfan@outlook.com>
2020-09-15 09:39:54 +0200

    Removed testing code

--- host.py
+++ host.py
@@ -29,34 +29,6 @@ def main():
     # Set up validators
     ui.setUpValidators()
     ui.setUpMisc()
-    """
-    print("************** NB DEBUGGING SETTINGS ARE SET IN host.py ********************")
-    ui.game_obj.host.addPlayer(0)
-    ui.game_obj.host.players[0].firm_name = "testing"
-    ui.game_obj.host.players[0].firm_motto= "testing"
-    ui.game_obj.host.players[0].statistics.placement = 1
-    ui.game_obj.host.players[0].statistics.profits = 100000
-    ui.game_obj.host.players[0].statistics.emissions = 1000000
-    ui.game_obj.host.addPlayer(1)
-    ui.game_obj.host.players[1].firm_name = "Statkraft"
-    ui.game_obj.host.players[1].firm_motto = "Grønn som faen"
-    ui.game_obj.host.players[1].statistics.placement = 2
-    ui.game_obj.host.players[1].statistics.profits = 90000
-    ui.game_obj.host.players[1].statistics.emissions = 140000
-    
-    plant = Plant("PV", "testingplant", 500, 10000, 0.5, 1000, 20, 10, 0)
-    plant2 = Plant("PV", "secondplant", 500, 10000, 0.5, 1000, 20, 10, 1)
-    bid = Bid(0, plant, 500, 500)
-    ui.game_obj.host.players[0].appendPlant(plant)
-    bids = dict(plant=[0, 0, 0, 0], actual_amount=[200, 300, 0, 0], bid_amount=[500, 300, 100, 50],
-                profits=[200, 300, 0, 0])
-    results = dict(year=1, round=1, profits=100, revenue=200, cost=80, taxes=20, emissions=50, bids=bids)
-    ui.game_obj.host.getPlayer(0).statistics.create_round_results(results)
-    results2 = dict(year=1, round=2, profits=-100, revenue=0, cost=-100, taxes=0, emissions=0,
-                    bids=dict(plant=[], actual_amount=[], bid_amount=[], profits=[]))
-    ui.game_obj.host.getPlayer(0).statistics.create_round_results(results2)
-    """
-
     #ui.setUpImages()
     # MainWindow.setWindowState(QtCore.Qt.WindowFullScreen)
     MainWindow.show()
